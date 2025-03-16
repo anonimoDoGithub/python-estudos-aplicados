7235c942eb7b03c79807595ff8b43f057dd63648
anonimoDoGithub <xxexpgamerxx@gmail.com>
2025-03-15 23:26:47 -0300

    02 escrita

--- manipulando arquivos/01_escrita.py
+++ manipulando arquivos/01_escrita.py
@@ -1,5 +1,5 @@
 print("Lista de Convidados \n")
-name = print("Digite o nome que deseja adicionar: \n")
+convidados = input("Digite o nome que deseja adicionar: \n")
 
 """
 Arquivos:
@@ -7,6 +7,8 @@ Arquivos:
 2 - opção a - append
 3 - opção r - read
 """
-file = open("convidados.txt", "w") #abrindo arquivo com o metodo open("colocando o nome do arquivo", "colocando o modo: w, a ou r")
-file.write(name)
+#abrindo arquivo com o metodo open("colocando o nome do arquivo", "colocando o modo: w, a ou r")
+#file = open("convidados.txt", "w") #como não tem nenhum arquivo ainda criado, precisamos criar primeiro usando o "w"
+file = open("convidados.txt", "a") #logo em seguida para adicionar nomes usamos o "a"
+file.write(f"{convidados} \n") #usando o F para formatar a string, colocando uma quebra de linha com o \n
 file.close() #fechando o arquivo, sempre lembrar de fechar o arquivo com close()
